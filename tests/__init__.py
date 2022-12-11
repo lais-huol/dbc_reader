8ebc968d5c9655dfc2f2d4b682a505897caa0a5d
KelsonCM <kelsoncm@gmail.com>
2022-12-11 17:21:20 -0300

    test: 100% coverage

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,24 +10,28 @@ PATH_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 class TestDbfReader(unittest.TestCase):
 
+    def test_file_not_exists(self):
+        with self.assertRaises(FileNotFoundError):
+            DbcReader("tests/data/filenotexsits.dbc")
+
     def test_read(self):
-        with DbcReader("tests/data/EEAL0703.dbc") as dbc:
-            print(DbfDescriptionText(dbc))
-            self.assertEqual(dbc.actual_record, 0)
-            self.assertEqual(dbc.file_size, 9286)
-            self.assertEqual(dbc.records, 14)
-            self.assertEqual(dbc.last_update, datetime.date(1905, 7, 13))
-            self.assertIsNotNone(dbc.definition)
-            self.assertEqual(dbc.definition.reader, dbc)
-            self.assertEqual(dbc.definition.encoding, 'iso-8859-1')
-            self.assertEqual(dbc.definition.dbf_format, 3)
-            self.assertEqual(dbc.definition.headerlen, 1025)
-            self.assertEqual(dbc.definition.numfields, 31)
-            self.assertEqual(dbc.definition.record_size, 590)
-            self.assertIsNotNone(dbc.definition.fields)
-            self.assertEqual(dbc.definition.terminator, b'\r')
+        with open("tests/data/EEAL0703.txt") as txt:
+            with DbcReader("tests/data/EEAL0703.dbc") as dbc:
+                self.assertIsNotNone(dbc.definition)
+                self.assertEqual(DbfDescriptionText(dbc.definition).__str__(), txt.read())
+                self.assertEqual(dbc.actual_record, 0)
+                self.assertEqual(dbc.records, 1)
+                self.assertEqual(dbc.last_update, datetime.date(2010, 3, 28))
+                self.assertEqual(dbc.definition.reader, dbc)
+                self.assertEqual(dbc.definition.encoding, 'iso-8859-1')
+                self.assertEqual(dbc.definition.dbf_format, 3)
+                self.assertEqual(dbc.definition.headerlen, 993)
+                self.assertEqual(dbc.definition.numfields, 30)
+                self.assertEqual(dbc.definition.record_size, 179)
+                self.assertIsNotNone(dbc.definition.fields)
+                self.assertEqual(dbc.definition.terminator, b'\r')
 
     def test_iterate(self):
-        with DbcReader("tests/data/EEAL0703.dbc", 'rb') as dbc:
+        with DbcReader("tests/data/EEAL0703.dbc") as dbc:
             [row for row in dbc]
             self.assertEqual(dbc.actual_record, dbc.records)
